c0af4c706ee04a4944a0a696f3ab2febc0c4c380
Vinícius Dias <vintheman@gmail.com>
2018-04-22 18:00:17 -0300

    Commit até aula 01, atividade 03.

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,17 +1,26 @@
 class ListaNegociacoes {
-    
-    constructor() {
-        
+
+    constructor(contexto, capturador) {
+
         this._negociacoes = [];
+        this._capturador = capturador;
+        this._contexto = contexto;
     }
-    
+
     adiciona(negociacao) {
-        
+
         this._negociacoes.push(negociacao);
+        Reflect.apply(this._capturador, this._contexto, [this]);
     }
-    
+
     get negociacoes() {
-        
+
         return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+
+        this._negociacoes = [];
+        Reflect.apply(this._capturador, this._contexto, [this]);
     }
 }
