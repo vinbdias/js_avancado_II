a3743f49535abe000f1f7bec31e9ed3e8e3c7f81
vinbdias <vintheman@gmail.com>
2018-04-25 12:37:27 -0300

    Commit até aula 05, atividade 01.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -31,30 +31,19 @@ class NegociacaoController {
 
     importaNegociacoes() {
 
-        let xhr = new XMLHttpRequest();
+        let service = new NegociacaoService();
 
-        xhr.open('GET', 'negociacoes/semana');
 
-        xhr.onreadystatechange = () => {
+        service.obterNegociacoesDaSemana((erro, negociacoes)  => {
+            if(erro) {
 
-            if(xhr.readyState == 4) {
-
-                if(xhr.status == 200) {
-                    
-                    JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
-                        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-                        this._mensagem.texto = 'Negociações importadas com sucesso.';
-                }
-                else {
-                    
-                    console.log(xhr.responseText);
-                    this._mensagem.texto = 'Não foi possível obter as negociações do servidor.';
-                }
+                this._mensagem.texto = erro;
+                return;
             }
-        };
 
-        xhr.send();
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações importadas com sucesso.';
+        });
     }
 
     apaga() {
